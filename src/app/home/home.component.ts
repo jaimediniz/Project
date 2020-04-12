b58ecd0a7d9781154ad6610d8952101391a7f5bb
Jaime Diniz <jaime@alunos.utfpr.edu.br>
2020-04-12 13:47:21 +0200

    feat: Add home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { userService } from "../services/user.service";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
+})
+export class HomeComponent implements OnInit {
+  users: Array<{ id: number; name: string }>;
+
+  constructor(private userService: userService) {
+    this.userService.usersSub.asObservable().subscribe((users) => {
+      this.users = users;
+    });
+  }
+
+  ngOnInit(): void {}
+
+  selectTarget(userID): void {
+    this.userService.emitSelected(this.users[userID]);
+  }
+}
