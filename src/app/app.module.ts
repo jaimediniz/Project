e693bf4fa3b1ae0efd69ddb7013cdda5ca075c28
Jaime Diniz <jaime@alunos.utfpr.edu.br>
2020-04-12 13:44:15 +0200

    feat: Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContactsComponentComponent } from './home/drawer/contacts/contacts-component/contacts-component.component';
-import { ContactsComponent } from './home/drawer/contacts/contacts.component';
-import { InvitesComponent } from './home/drawer/invites/invites.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ContactsComponent } from "./home/drawer/contacts/contacts.component";
+import { InvitesComponent } from "./home/drawer/invites/invites.component";
+import { DrawerComponent } from "./home/drawer/drawer.component";
+import { ViewerComponent } from "./home/viewer/viewer.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
+import { HomeComponent } from "./home/home.component";
+import { UserInfoComponent } from "./home/viewer/user-info/user-info.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsComponentComponent,
     ContactsComponent,
-    InvitesComponent
+    InvitesComponent,
+    DrawerComponent,
+    ViewerComponent,
+    HomeComponent,
+    UserInfoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MaterialModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
