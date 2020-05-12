dd35fc6ed4569519007cd1e5ff2a2909de42cc1e
Jaime Diniz <j_diniz4@hotmail.com>
2020-05-12 23:10:15 +0200

    feat: Connect to notificatons service

--- src/app/home/drawer/side-bar/side-bar.component.ts
+++ src/app/home/drawer/side-bar/side-bar.component.ts
@@ -6,6 +6,9 @@ import {
   OnDestroy,
 } from "@angular/core";
 import { OpenViewerService } from "src/app/services/open-viewer.service";
+import { NotificationsService } from "src/app/services/notifications.service";
+import { Subject } from "rxjs";
+import { LoggerService } from "src/app/services/logger.service";
 
 @Component({
   selector: "app-side-bar",
@@ -13,13 +16,36 @@ import { OpenViewerService } from "src/app/services/open-viewer.service";
   styleUrls: ["./side-bar.component.scss"],
 })
 export class SideBarComponent implements OnInit, OnDestroy {
+  webPackage = "./src/app/home/drawer/side-bar/side-bar.component.ts";
+
   @Output() open = new EventEmitter<number>();
+
+  private muteAudioSub: any; // Subject
   public mute = false;
   public contactsUnreadMessages: number = 8;
   public groupsUnreadMessages: number = 9;
   public invitesUnreadMessages: number = 5;
 
-  constructor(private openViewerService: OpenViewerService) {}
+  constructor(
+    private openViewerService: OpenViewerService,
+    private notificationsService: NotificationsService,
+    private logger: LoggerService
+  ) {
+    this.mute = this.notificationsService.muteAudio;
+    this.muteAudioSub = this.notificationsService.muteAudioSub
+      .asObservable()
+      .subscribe((muteAudio) => {
+        this.mute = muteAudio;
+      });
+    this.muteAudioSub.subscriberName = "SideBarComponent";
+
+    this.logger.functionLog({
+      webPackage: this.webPackage,
+      className: "SideBarComponent",
+      functionName: "constructor",
+      values: ["Subscribed to audio status"],
+    });
+  }
 
   ngOnInit(): void {}
 
@@ -27,5 +53,19 @@ export class SideBarComponent implements OnInit, OnDestroy {
     this.openViewerService.emitView(view);
   }
 
-  ngOnDestroy(): void {}
+  toggleMute() {
+    this.notificationsService.emitAudioMuted(!this.mute);
+  }
+
+  ngOnDestroy(): void {
+    this.muteAudioSub.unsubscribe();
+    this.notificationsService.emitAudioMuted(false);
+
+    this.logger.functionLog({
+      webPackage: this.webPackage,
+      className: "SideBarComponent",
+      functionName: "ngOnDestroy",
+      values: ["Unsubscribed from audio status"],
+    });
+  }
 }
