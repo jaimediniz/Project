dbc8bbdc3fa350bd05b65efd344db7d23ee22bcd
Jaime Diniz <j_diniz4@hotmail.com>
2020-05-12 20:51:15 +0200

    feat: Add log when emiting values

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
+import { LoggerService } from "./logger.service";
 
 @Injectable({
   providedIn: "root",
@@ -12,14 +13,30 @@ export class UserService {
   selectedUserSub = new Subject<{ id: number; name: string }>();
 
   emitUsers(users: Array<{ id: number; name: string }>): void {
+    this.logger.emitLog({
+      className: "UserService",
+      functionName: "emitUsers",
+      description: "Emit new Array<{ id: number; name: string }>",
+      variable: "users",
+      value: users,
+      subscribers: this.usersSub.observers,
+    });
     this.users = users;
     this.usersSub.next(users);
   }
 
   emitSelected(selectedUser: { id: number; name: string }): void {
+    this.logger.emitLog({
+      className: "UserService",
+      functionName: "emitSelected",
+      description: "Emit new { id: number; name: string }",
+      variable: "selectedUser",
+      value: selectedUser,
+      subscribers: this.selectedUserSub.observers,
+    });
     this.selectedUser = selectedUser;
     this.selectedUserSub.next(selectedUser);
   }
 
-  constructor() {}
+  constructor(private logger: LoggerService) {}
 }
