0326d096402250352c6f47e43770327a8b753652
Jaime Diniz <j_diniz4@hotmail.com>
2020-05-12 23:08:59 +0200

    feat: Add notifications service

---
+++ src/app/services/notifications.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { LoggerService } from "./logger.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class NotificationsService {
+  muteAudio = false;
+  muteAudioSub = new Subject<boolean>();
+
+  emitAudioMuted(muteAudio: boolean): void {
+    this.logger.emitLog({
+      className: "NotificationsService",
+      functionName: "emitAudioChange",
+      description: "Emit new boolean value",
+      variable: "muteAudio",
+      value: muteAudio,
+      subscribers: this.muteAudioSub.observers,
+    });
+    this.muteAudio = muteAudio;
+    this.muteAudioSub.next(muteAudio);
+  }
+
+  constructor(private logger: LoggerService) {}
+}
