362752a7c991757d9b253c2077985acc0d329e8c
Jaime Diniz <j_diniz4@hotmail.com>
2020-05-13 02:24:18 +0200

    feat: Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { InvitesComponent } from "./home/drawer/invites/invites.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: InvitesComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "chat", component: InvitesComponent },
+  { path: 'chat', loadChildren: () => import('./routes/chat/chat.module').then(m => m.ChatModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
