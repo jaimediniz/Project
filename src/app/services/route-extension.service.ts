bb48e106a39c5c67c4217ee3adaaef95b3acf194
Jaime Diniz <j_diniz4@hotmail.com>
2020-05-13 03:41:26 +0200

    feat: Redirect to home onSameUrlNavigation

--- src/app/services/route-extension.service.ts
+++ src/app/services/route-extension.service.ts
@@ -1,25 +1,60 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { LoggerService } from "./logger.service";
 import { Subject } from "rxjs";
+
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Params,
+  Router,
+} from "@angular/router";
+
+import { LoggerService } from "./logger.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class RouteExtensionService {
-  queryParamsSubject = new Subject();
+  lastRoute: string;
+  paramsSubject = new Subject();
 
-  constructor(private logger: LoggerService, private route: ActivatedRoute) {
-    this.route.queryParams.subscribe((params) => {
-      this.logger.emitLog({
-        className: "RouteExtensionService",
-        functionName: "constructor",
-        description: "Emit new {[key: string]: any;};",
-        variable: "params",
-        value: params,
-        subscribers: this.queryParamsSubject.observers,
-      });
-      this.queryParamsSubject.next(params);
+  constructor(private logger: LoggerService, private route: Router) {
+    this.route.onSameUrlNavigation = "reload";
+    this.route.events.subscribe(async (event) => {
+      if (event instanceof NavigationStart) {
+        // Show loading indicator
+      }
+
+      if (event instanceof NavigationEnd) {
+        // Hide loading indicator
+
+        if (this.lastRoute === event.urlAfterRedirects) {
+          await this.route.navigate(["/home"]);
+          return;
+        }
+        this.logger.emitLog({
+          className: "RouteExtensionService",
+          functionName: "constructor",
+          description: "Emit new string",
+          variable: "urlAfterRedirects",
+          value: event.urlAfterRedirects,
+          subscribers: this.paramsSubject.observers,
+        });
+        this.lastRoute = event.urlAfterRedirects;
+        this.paramsSubject.next(event.urlAfterRedirects);
+      }
+
+      if (event instanceof NavigationError) {
+        // Hide loading indicator
+        this.logger.errorLog({
+          className: "RouteExtensionService",
+          functionName: "constructor",
+          description: "Navigation Error!",
+          variable: "Error",
+          value: event.error,
+        });
+      }
     });
   }
 }
