8649aa56fc801cf692b9d0d3c9b62cc097067ea5
Jaime Diniz <j_diniz4@hotmail.com>
2020-05-19 01:19:54 +0200

    feat: Add onResized to viewer

--- src/app/components/viewer/viewer.component.ts
+++ src/app/components/viewer/viewer.component.ts
@@ -2,7 +2,8 @@ import { Subscription } from "rxjs";
 import { LoggerService } from "src/app/services/logger.service";
 import { RouteExtensionService } from "src/app/services/route-extension.service";
 
-import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit, Output } from "@angular/core";
+import { ViewerService } from "src/app/services/viewer-service.service";
 
 @Component({
   selector: "app-viewer",
@@ -16,9 +17,15 @@ export class ViewerComponent implements OnInit, OnDestroy {
   selectedViewerSub: any;
   activatedRoute: any; // Subscription
 
+  width: number = window.innerWidth;
+  height: number = window.innerHeight;
+  mobileWidth: number = 768;
+  isMobile: boolean = false;
+
   constructor(
     private logger: LoggerService,
-    private route: RouteExtensionService
+    private route: RouteExtensionService,
+    private viewerService: ViewerService
   ) {}
 
   ngOnInit(): void {
@@ -35,6 +42,14 @@ export class ViewerComponent implements OnInit, OnDestroy {
       functionName: "ngOnInit",
       values: ["Subscribed to route"],
     });
+  }
+
+  onResized(event) {
+    this.width = event.newWidth;
+    if (this.isMobile !== this.width < this.mobileWidth) {
+      this.isMobile = this.width < this.mobileWidth;
+      this.viewerService.emitIsMobile(this.isMobile);
+    }
   }
 
   ngOnDestroy(): void {
