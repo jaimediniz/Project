2a7bf5286e54af075f8e53086ddcc761eb4e102f
Jaime Diniz <j_diniz4@hotmail.com>
2020-05-30 23:32:41 +0200

    feat: Add login to home page as template

--- src/app/components/viewer/dashboard/components-collection/components-collection.component.ts
+++ src/app/components/viewer/dashboard/components-collection/components-collection.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MobileService } from "src/app/services/mobile.service";
+import { LoggerService } from "src/app/services/logger.service";
 
 @Component({
-  selector: 'app-components-collection',
-  templateUrl: './components-collection.component.html',
-  styleUrls: ['./components-collection.component.scss']
+  selector: "app-components-collection",
+  templateUrl: "./components-collection.component.html",
+  styleUrls: ["./components-collection.component.scss"],
 })
 export class ComponentsCollectionComponent implements OnInit {
+  webPackage =
+    "./src/app/components/viewer/dashboard/components-collection.component.ts";
 
-  constructor() { }
+  isMobileSub: any;
+  isMobile: boolean;
 
-  ngOnInit(): void {
+  constructor(
+    private logger: LoggerService,
+    private mobileService: MobileService
+  ) {
+    this.isMobileSub = this.mobileService.isViewerMobileSub.subscribe(
+      (isViewerMobile) => {
+        this.isMobile = isViewerMobile;
+      }
+    );
+    this.isMobileSub.subscriberName = "ComponentsCollectionComponent";
+
+    this.logger.functionLog({
+      webPackage: this.webPackage,
+      className: "ComponentsCollectionComponent",
+      functionName: "constructor",
+      values: ["Subscribed to viewer isMobile"],
+    });
   }
 
+  ngOnInit(): void {}
 }
